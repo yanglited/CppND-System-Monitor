e55157d20fa306ab62756c12d054e20facd1f569
Yang Li <yang.li@alertinnovation.com>
2020-12-14 21:41:47 -0500

    Improved up time printing format with padding zeros.

--- src/format.cpp
+++ src/format.cpp
@@ -1,23 +1,23 @@
-#include <string>
-
 #include "format.h"
+
+#include <iomanip>
+#include <sstream>
+#include <string>
 
 using std::string;
 
 string Format::ElapsedTime(long seconds) {
-
   // Initialize hours, minutes, and seconds used for return:
-  long hh = seconds / 3600;;
+  long hh = seconds / 3600;
   long mm = (seconds - hh * 3600) / 60;
   long ss = seconds - hh * 3600 - mm * 60;
 
-  std::string upTimeString;
-  upTimeString.append(std::to_string(hh));
-  upTimeString.append(":");
-  upTimeString.append(std::to_string(mm));
-  upTimeString.append(":");
-  upTimeString.append(std::to_string(ss));
+  std::ostringstream upTimeStringStream;
+  upTimeStringStream << std::setfill('0') << std::setw(2) << std::to_string(hh);
+  upTimeStringStream << ":";
+  upTimeStringStream << std::setfill('0') << std::setw(2) << std::to_string(mm);
+  upTimeStringStream << ":";
+  upTimeStringStream << std::setfill('0') << std::setw(2) << std::to_string(ss);
 
-  //TODO need to pad a zero when single digit numbers appear.
-
-  return upTimeString; }
+  return upTimeStringStream.str();
+}
